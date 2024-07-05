3ff65f663c9f296235193e2590b4a8df09f67e88
R Vikrama Simha Reddy <120121115+Vikram491@users.noreply.github.com>
2024-07-05 18:09:36 +0530

    Create Lecture 5- cipherschools.java

---
+++ Lecture 5- cipherschools.java
@@ -0,0 +1,10 @@
+public class Lec5 {
+   public static void main(Stirng args[]){
+     int m = 8;
+     int n = m++;
+     System.out.println("M is : "+m+", N is : "+n);
+     int v = 4;
+     int s = ++v;
+     System.out.println("V is : "+v+", S is : "+s);
+   }
+}

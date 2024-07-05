bef5536787b5d031d881723242978a80ff981618
R Vikrama Simha Reddy <120121115+Vikram491@users.noreply.github.com>
2024-07-05 16:46:40 +0530

    Create Lecture 3- cipherschools.java

---
+++ Lecture 3- cipherschools.java
@@ -0,0 +1,14 @@
+public class Lec3 {
+    public static void main(String args[]) {
+        int x = 6;
+        int y = 7;
+        System.out.println("X is : "+x);
+        System.out.println("Y is : "+y);
+        byte b = 10;
+        System.out.println("B is : "+b);
+        byte b2 = 128;
+        System.out.println("B2 is : "+b2);
+        int v = b;
+        System.out.println("V is : "+v);
+    }
+}
